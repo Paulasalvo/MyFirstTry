87ab53fe02633fbb04d56b498ff364e1331270c2
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-13 03:19:07 -0300

    Initial commit

--- app/src/main/java/com/namkuzo/myfirsttry/MainActivity.kt
+++ app/src/main/java/com/namkuzo/myfirsttry/MainActivity.kt
@@ -3,12 +3,18 @@ package com.namkuzo.myfirsttry
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.namkuzo.myfirsttry.ui.theme.MyFirstTryTheme
 
@@ -16,22 +22,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Paly")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            )
+
+            {
+                Text(text = "Hello")
+                Text(text = "World")
+            }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-        Greeting("Android")
-
 }
