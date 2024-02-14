df36f917b8978626df580aa0832b8140c20e6a8a
Paulasalvo <p.arroyosalvo@gmail.com>
2024-02-13 21:46:28 -0800

    part 2

--- app/src/main/java/com/namkuzo/myfirsttry/MainActivity.kt
+++ app/src/main/java/com/namkuzo/myfirsttry/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .width(300.dp)
+                    .height(300.dp)
                     .background(Color.Green),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceAround
